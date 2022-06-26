8f7dfd899e143354195f6400ab47eb1c0e98e29e
Andre Fernando Bezada <a.bezada.maza@gmail.com>
2022-06-26 14:12:35 -0500

    Initial Commit

---
+++ ToDoDemo/ToDoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoDemoApp.swift
+//  ToDoDemo
+//
+//  Created by Andre Bezada on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
